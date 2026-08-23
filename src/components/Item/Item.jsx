83018fc395aee8ec03981789c5e7ec32f5359f9a
Ronald Vargas Camacho <ronald140694@gmail.com>
2026-08-23 14:12:30 -0500

    añadiendo etiqueta de stock en Item

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,9 +1,19 @@
+import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 
 export function Item({ product }) {
   return (
     <Link to={`/product/${product.id}`}>
-      <article className='flex flex-col gap-2 overflow-hidden rounded-3xl bg-white cursor-pointer'>
+      <article className='flex flex-col gap-2 overflow-hidden rounded-3xl bg-white cursor-pointer relative'>
+        <span
+          className={clsx(
+            'absolute top-3 right-2 p-1 text-[10px] text-white text-center  rounded-lg z-1',
+            product.stock > 0 ? 'bg-green-700' : 'bg-red-500',
+          )}
+        >
+          {product.stock > 0 ? 'En stock' : 'Sin stock'}
+        </span>
+
         <div className='h-64 w-full rounded-3xl overflow-hidden'>
           <img
             src={product.image}
